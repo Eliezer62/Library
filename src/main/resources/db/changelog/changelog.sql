3d5eb0d00d40464436a9440c16ea616843a19d34
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-02-04 17:00:57 -0300

    add migration to books

--- src/main/resources/db/changelog/changelog.sql
+++ src/main/resources/db/changelog/changelog.sql
@@ -1,1 +1,15 @@
 -- liquibase formatted sql
+-- changeset eliezer:1
+CREATE TABLE IF NOT EXISTS books (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    isbn VARCHAR(13),
+    ean VARCHAR(13),
+    publisher VARCHAR(255),
+    publisher_date DATE,
+    pages INT,
+    synopsis TEXT,
+    CONSTRAINT pk_books PRIMARY KEY(id),
+    CONSTRAINT uc_books_page_positive CHECK (pages > 0)
+) ENGINE = InnoDB;
+
+-- rollback DROP TABLE books;
