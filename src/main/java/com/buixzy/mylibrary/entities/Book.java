6ce6b1842ab70e6a80d1e6f1d909d91985d59481
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-02-04 17:01:26 -0300

    add entity Book

---
+++ src/main/java/com/buixzy/mylibrary/entities/Book.java
@@ -0,0 +1,44 @@
+package com.buixzy.mylibrary.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "books")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "isbn", columnDefinition = "VARCHAR(13)", length = 13)
+    private String ISBN;
+
+    @Column(name = "ean", columnDefinition = "VARCHAR(13)", length = 13)
+    private String EAN;
+
+    @Column(name = "publisher", columnDefinition = "VARCHAR(255)", length = 255)
+    private String publisher;
+
+    @Column(name = "publisher_date", columnDefinition = "DATE")
+    private Date publisherDate;
+
+    @Column(name = "pages", columnDefinition = "INT")
+    private Integer pages;
+
+    @Column(name = "synopsis", columnDefinition = "TEXT")
+    private String synopsis;
+}
