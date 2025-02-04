c4316dcef94f66dbf9ab9d790e567e720463fa1b
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-02-04 18:04:47 -0300

    add bean repository

--- src/main/java/com/buixzy/mylibrary/repositories/BookRepository.java
+++ src/main/java/com/buixzy/mylibrary/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package com.buixzy.mylibrary.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.buixzy.mylibrary.entities.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
--- src/main/java/com/buixzy/mylibrary/repositories/CopyRepository.java
+++ src/main/java/com/buixzy/mylibrary/repositories/CopyRepository.java
@@ -1,9 +1,11 @@
 package com.buixzy.mylibrary.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.buixzy.mylibrary.entities.Copy;
 
+@Repository
 public interface CopyRepository extends JpaRepository<Copy, Long> {
 
 }
