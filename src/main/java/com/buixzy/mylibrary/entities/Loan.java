d0a5ac8c66a53807bac2686e1eb38f467c55959f
Eliezer de Almeida <eliezerdealmeida62@gmail.com>
2025-02-10 02:11:14 -0300

    add loan property copy and user

--- src/main/java/com/buixzy/mylibrary/entities/Loan.java
+++ src/main/java/com/buixzy/mylibrary/entities/Loan.java
@@ -1,10 +1,13 @@
 package com.buixzy.mylibrary.entities;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.buixzy.mylibrary.converters.StatusLoanConverter;
 import com.buixzy.mylibrary.enums.StatusLoan;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
@@ -49,5 +52,27 @@ public class Loan {
     @ManyToOne
     @JsonIgnore
     private User user;
+
+    @JsonProperty("copy")
+    private Map<String, Object> copyJson() {
+        return new HashMap<>(Map.of(
+            "id", copy.getId(),
+            "book", copy.getBook().getName(),
+            "author",copy.getBook().getAuthor(),
+            "isbn", copy.getBook().getISBN(),
+            "code", copy.getCode()
+        ));
+    }
+
+    @JsonProperty("user")
+    private Map<String, Object> userJson() {
+        return new HashMap<>(Map.of(
+            "id", user.getId(),
+            "name", user.getName(),
+            "cpf", user.getCpf(),
+            "cardId", user.getCardId(),
+            "statusUser", user.getStatus().getStatusName()
+        ));
+    }
 
 }
